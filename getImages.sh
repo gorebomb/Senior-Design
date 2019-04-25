1828d029cd8b1bf342cb2b8cd3d700b87e5b2298
tushar <0tushar0@gmail.com>
2019-04-25 18:07:29 -0500

    updated script

--- getImages.sh
+++ getImages.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 scp pi@10.159.41.193:/home/pi/Downloads/srDesign/Senior-Design/edges.jpg .
+
+#expect "pi@10.159.41.193's password:\r"
+#send -- "raspberry\r"
+
 scp pi@10.159.41.193:/home/pi/Downloads/srDesign/Senior-Design/cam2.jpeg .
 
+#expect "pi@10.159.41.193's password:\r"
+#send -- "raspberry\r"
+
+read -p "enter count num: " count
+echo $count
+
+mv edges.jpg ./edges/$count.jpg
+mv cam2.jpeg ./pic/$count.jpeg

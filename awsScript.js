d32bbab67735d60c013f9621c3a5c5d603879448
tushar <0tushar0@gmail.com>
2019-04-25 01:00:40 -0500

    Rpi Files

---
+++ awsScript.js
@@ -0,0 +1,11 @@
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  //Open a file on the server and return its content:
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);

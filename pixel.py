b7701e0611145917bad68d1ef1ab5089d14c523a
0tushar0 <0tushar0@gmail.com>
2019-04-29 00:55:15 -0500

    finding pixels

--- pixel.py
+++ pixel.py
@@ -3,8 +3,21 @@ import numpy as np
 
 img = cv2.imread('highresedges.jpg')
 
-for i in range(0,20):
+print(img[700,700])
 
-	img[100+i,100] = [57,225,20]
-	img[100,100+i] = [57,225,20]
+"""
 
+for i in range(0,300):
+
+	img[500+i,500] = [57,225,20]
+	#print(img[700+i,700])
+	img[500,500+i] = [57,225,20]
+	#print(img[700,700+i])
+#cv2.imwrite('highresedges.jpg')
+
+"""
+
+img[565:600, 565:600] = (57,225,20)
+cv2.imshow('title', img)
+cv2.waitKey(0)
+#cv2.destroyWindow(img)

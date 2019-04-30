4234f12bb3a7cba2c1728aed5c850bf19e53cd33
Shubham <shubham.gore@gmail.com>
2019-04-29 19:45:16 -0500

    modified demo script

--- demo.sh
+++ demo.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-ssh pi@10.159.41.193 'bash -s' < pic.sh
-scp pi@10.159.41.193:/home/pi/edges.jpg .
-scp pi@10.159.41.193:/home/pi/cam2.jpeg .
+for i in {1..10}
+do
+	clear
+	#tput cup 5 5
+	date
+	#tput cup 6 5
+	ssh pi@10.159.41.193 'bash -s' < pic.sh
+	scp pi@10.159.41.193:/home/pi/edges.jpg .
+	scp pi@10.159.41.193:/home/pi/cam2.jpeg .
+	sleep 5s
+done
 
-#exit
